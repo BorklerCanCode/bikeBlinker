f392a57a2ecdb135219ee19b082b8bf9c43ba617
BorklerCanCode <151806915+BorklerCanCode@users.noreply.github.com>
2024-02-10 13:48:23 -0800

    Create lifeBlinkerGyro.ino
    feature overview

---
+++ lifeBlinkerGyro.ino
@@ -0,0 +1,9 @@
+////This version of the Life-Blinker is hardware+software
+//The lifeBlinkerGyro adds a decelerometer to light up the three LEDS
+//during a braking event, without a the need to add a traditional brake sensor/harness.
+
+////Code in progress, pending results of gyro hardware test
+//ic2 based gryo vs
+//analog gyro
+//eta Q1-Q2 2024, file a bug report against this .ino if you want to see it sooner!
+//I will otherwise prioritize other features based on various constraints. -JB
