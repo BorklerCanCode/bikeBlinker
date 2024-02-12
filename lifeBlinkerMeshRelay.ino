ee41631bbeb57a66dfd79585c8453b69ba958057
BorklerCanCode <151806915+BorklerCanCode@users.noreply.github.com>
2024-02-11 23:44:44 -0800

    Create lifeBlinkerMeshRelay.ino

---
+++ lifeBlinkerMeshRelay.ino
@@ -0,0 +1,9 @@
+////This version of the Life-Blinker is hardware+software, and uses 2 or more Life Blinkers.
+//The lifeBlinkerMeshRelay uses a mesh network to extend signaling over multiple nodes,
+//for up to a mile (range tests and max node count pending).
+//This variant can be used to communicate alerts, or send Morse code wirelessly.
+//Adding a momentary switch may be desirable for your use case.
+
+////Code in progress, pending mesh mode debugging.
+//eta Q1-Q2 2024, file a bug report against this .ino if you want to see it sooner!
+//I will otherwise prioritize other features based on various constraints. -JB
