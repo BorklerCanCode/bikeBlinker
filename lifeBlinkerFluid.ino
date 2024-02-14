6e85d8c13728b0f68be226ee51520bcb0d585dfd
BorklerCanCode <151806915+BorklerCanCode@users.noreply.github.com>
2024-02-13 20:01:00 -0800

    Create lifeBlinkerFluid.ino
    vote up your favorite work in proigress ino with a bug report!

---
+++ lifeBlinkerFluid.ino
@@ -0,0 +1,7 @@
+////lifeBlinkerFluid is hardware and software variant for small watercraft and kayakers
+//IP68 rated light protoype with magnetic switch(es) to change from marker mode to sos mode.
+//Alternative switch may be a metal ball-trap switch to indicate unconscious pilot in water. 
+
+////WIP/pending summer battery project with UCSD may provide the perfect sealable power source.
+//eta Q4 2024 to Q2 2025, file a bug report against this .ino if you want to see it sooner!
+//I will otherwise prioritize other features based on various constraints. -JB
